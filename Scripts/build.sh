a3b498d7dcc0454d20080a7d305b4ea91562fcaf
Henrique Dias <diaslinoh@gmail.com>
2018-05-21 16:52:50 -0300

    Travis Release

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -45,6 +45,11 @@ echo "Attempting to build $project for WebGL"
   -quit
 '
 
-757396
 echo 'Logs from build'
 cat $(pwd)/unity.log
+
+
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
