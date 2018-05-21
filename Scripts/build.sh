0d5a61cc0683766ec0f59b3cdfd68368f4dcd90d
Henrique Dias <diaslinoh@gmail.com>
2018-05-21 08:08:35 -0300

    Travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,17 +2,6 @@
 
 project="unity-aa"
 
-: 'Multiline comment:
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
-
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -32,8 +21,19 @@ echo "Attempting to build $project for Linux"
   -projectPath $(pwd) \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
-'
 
+: 'Multiline comment:
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
+  
 echo "Attempting to build $project for WebGL"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -43,6 +43,7 @@ echo "Attempting to build $project for WebGL"
   -projectPath $(pwd) \
   -buildTarget WebGL \
   -quit
+'
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
