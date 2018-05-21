5304a0c46786f8858368b28df8141ba2ab59efe1
Henrique Dias <diaslinoh@gmail.com>
2018-05-21 14:53:03 -0300

    Travis

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 
+#BASE_URL=http://netstorage.unity3d.com/unity
+#HASH=d4d99f31acba
+#VERSION=2018.1.0f2
+
 BASE_URL=http://netstorage.unity3d.com/unity
-HASH=d4d99f31acba
-VERSION=2018.1.0f2
+HASH=649f48bbbf0f
+VERSION=5.4.1f1
 
 download() {
   file=$1
@@ -23,7 +27,7 @@ install() {
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
 # http://netstorage.unity3d.com/unity/b8cbb5de9840/unity-2018.1.1f1-osx.ini 
-
+: 'Multiline comment:
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 #install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
 #install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
@@ -32,3 +36,9 @@ install "MacEditorTargetInstaller/UnitySetup-Mac-IL2CPP-Support-for-Editor-$VERS
 #install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-$VERSION.pkg"
+'
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
